aed6524d6d1170003b1ded663f71f083be113bf4
laxmivpatil <laxmi.patil@techverse.world>
2024-01-18 11:55:17 +0530

    This is second filebnnvnvbv

--- src/main/java/com/techverse/satya/Repository/TimeSlotRepository.java
+++ src/main/java/com/techverse/satya/Repository/TimeSlotRepository.java
@@ -19,34 +19,29 @@ public interface TimeSlotRepository extends JpaRepository<TimeSlot, Long> {
 		       "WHERE ts.date = :date AND tsd.startTime = :startTime")
 		String findAddressByDateAndStartTime(@Param("date") String date, @Param("startTime") String startTime);
 
-	@Query("SELECT tsd.address FROM TimeSlot t " +
+	/* @Query("SELECT tsd.address FROM TimeSlot t " +
 		       "JOIN t.timeSlotDetails tsd " +
 		       "WHERE t.date = :date AND :startTime BETWEEN tsd.startTime AND tsd.endTime AND t.admin.id = :adminId")
+		String findAddressByDateStartTimeAndAdminId(@Param("date") String date,
+		                                            @Param("startTime") String startTime,
+		                                            @Param("adminId") Long adminId); 
+	 
+*/
+	@Query("SELECT tsd.address FROM TimeSlot t " +
+		       "JOIN t.timeSlotDetails tsd " +
+		       "WHERE t.date = :date " +
+		       "AND FUNCTION('STR_TO_DATE', :startTime, '%h:%i%p') >= FUNCTION('STR_TO_DATE', tsd.startTime, '%h:%i%p') " +
+		       "AND FUNCTION('STR_TO_DATE', :startTime, '%h:%i%p') <= FUNCTION('STR_TO_DATE', tsd.endTime, '%h:%i%p') " +
+		       "AND t.admin.id = :adminId")
 		String findAddressByDateStartTimeAndAdminId(@Param("date") String date,
 		                                            @Param("startTime") String startTime,
 		                                            @Param("adminId") Long adminId);
 
 	
-	/*@Query("SELECT tsd.address FROM TimeSlot t " +
-		       "JOIN t.timeSlotDetails tsd " +
-		       "WHERE t.date = :date AND tsd.startTime = :startTime AND t.admin.id = :adminId")
-		String findAddressByDateStartTimeAndAdminId(@Param("date") String date, @Param("startTime") String startTime, @Param("adminId") Long adminId);
 	 
-	  */
 	  
 	  List<TimeSlot> findByAdminAndDate(Admin admin, String date);
-	/*imp
-	  @Query("SELECT tsd FROM TimeSlot t JOIN t.timeSlotDetails tsd WHERE t.admin = :admin AND t.date = :date " +
-		       "AND (" +
-		       "  (tsd.startTime < :newEndTime AND tsd.endTime > :newStartTime)" +
-		       "  OR (tsd.startTime >= :newStartTime AND tsd.startTime < :newEndTime)" +
-		       "  OR (tsd.endTime > :newStartTime AND tsd.endTime <= :newEndTime)" +
-		       ")")
-		List<TimeSlotDetail> findOverlappingTimeSlotDetails(@Param("admin") Admin admin,
-		                                                   @Param("date") String date,
-		                                                   @Param("newStartTime") String newStartTime,
-		                                                   @Param("newEndTime") String newEndTime);
-		                                                   */
+	 
 	  
 	  @Query("SELECT tsd FROM TimeSlot t JOIN t.timeSlotDetails tsd WHERE t.admin = :admin AND t.date = :date " +
 		       "AND (" +
