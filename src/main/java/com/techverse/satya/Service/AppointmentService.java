203e212aadd28ed5388fb52d9a182600a35b86b0
laxmivpatil <laxmi.patil@techverse.world>
2024-02-08 18:39:25 +0530

    This is second filgfjkjgkffffgkghfnn

--- src/main/java/com/techverse/satya/Service/AppointmentService.java
+++ src/main/java/com/techverse/satya/Service/AppointmentService.java
@@ -517,6 +517,7 @@ public class AppointmentService {
               smallerTimeSlot1.setStartTime(startTime.format(DateTimeFormatter.ofPattern("hh:mma")));
               smallerTimeSlot1.setEndTime(slotEndTime.format(DateTimeFormatter.ofPattern("hh:mma")));
               smallerTimeSlot1.setTimeSlot(timeSlot);
+              smallerTimeSlot1.setSlotBook(true);
               smallerTimeSlot1.setAdmin(appointment.getAdmin());
               smallerTimeSlot1.setAppointment(appointment);
               smallerTimeSlots.add(smallerTimeSlot1);
